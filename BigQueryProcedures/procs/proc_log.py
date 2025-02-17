3e0359597f93a85fedb9cbd0e18d6e01b527c1f6
João Paulo Cautter <jcautter@gmail.com>
2025-02-17 14:53:15 -0300

    Delete BigQueryProcedures/procs directory

--- BigQueryProcedures/procs/proc_log.py
+++
@@ -1,8 +0,0 @@
-class ProcLog:
-    def __init__(self):
-        pass
-
-    def __exec(self, title:str, query:str):
-        print("{title} -- Begin".format(title=title))
-        self.bq.execute_query(query, commit=True)
-        print("{title} -- Finish".format(title=title))

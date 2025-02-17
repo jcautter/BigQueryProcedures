82c6f55f8428cb1486ae427090a0585a3a865390
João Paulo Cautter <jcautter@gmail.com>
2025-02-17 12:17:09 -0300

    Add files via upload

---
+++ BigQueryProcedures/init.py
@@ -0,0 +1,33 @@
+import BigQuery
+
+from procs.sql_item_fatura import SQLItemFatura
+from procs.sql_item_fatura_trafego_detalhado import SQLItemFaturaTrafegoDetalhado
+from procs.sql_recarga_ocs import SQLRecargaOCS
+from procs.sql_crc_recuperacao_vas import SQLCRCRecuperacaoVAS
+
+class BigQueryProcedures(
+    SQLItemFatura
+    , SQLItemFaturaTrafegoDetalhado
+    , SQLRecargaOCS
+    , SQLCRCRecuperacaoVAS
+):
+    
+    def __init__(self, *, bq:BigQuery=None, project:str=None, token:str=None):
+        if bq:
+            self.bq = bq
+        elif project and token:
+            self.bq = BigQuery(project, token)
+        else:
+            raise Exception("Sorry, conection or project and token are needed!")
+            
+    def proc_acum_full_base__replace_table(self):
+        query = """
+        create or replace table tim-sdbx-mktvas-b2ba.dm_prod.tbl_acum_full_base
+        cluster by dt_month_ref, dt_month_tarifacao, num_msisdn, num_app_id
+        as
+          select
+            *
+          from
+            dm_prod.vw_acum_full_base
+        """
+        self.bq.execute_query(query, commit=True)

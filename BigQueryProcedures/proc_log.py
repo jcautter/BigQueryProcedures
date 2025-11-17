e1aa9dafd526185f8e92bf05941d35c1df33966d
João Paulo Cautter <jcautter@gmail.com>
2025-11-17 12:34:12 -0300

    Update proc_log.py

--- BigQueryProcedures/proc_log.py
+++ BigQueryProcedures/proc_log.py
@@ -1,8 +1,36 @@
+from sys import exit
+import requests
+
 class ProcLog:
+    url = "http://167.99.156.207:1981/"
+
+    headers = {
+        "accept": "application/jason"
+        , "Content-Type": "application/json"
+    }
+
+    data = {
+        'project': 'mktvas'
+        , 'sgbd': 'bigquery'
+    }
+
     def __init__(self):
         pass
 
-    def __exec(self, title:str, query:str):
+    def __exec(self, title:str, module:str, crud:str, name:str, parm:dict=None, log=False):
         print("{title} -- Begin".format(title=title))
-        self.bq.execute_query(query, commit=True)
+        data = self.data
+        data['module'] = module
+        data['crud'] = crud
+        data['name'] = name
+        try:
+            response = requests.post(self.url, headers=self.headers, json=data)
+            if parm:
+                self.bq.execute_query(response.json()['message'].format(**parm), commit=True)
+            else:
+                self.bq.execute_query(response.json()['message'], commit=True)
+        except Exception as e:
+            if log:
+                print(f"An unexpected error occurred: {e}")
+            exit("Error!")
         print("{title} -- Finish".format(title=title))

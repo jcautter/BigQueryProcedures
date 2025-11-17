a46d6d0b467baf147575ff61c097ac6750eab5b7
João Paulo Cautter <jcautter@gmail.com>
2025-11-17 12:41:00 -0300

    Update __init__.py

--- BigQueryProcedures/__init__.py
+++ BigQueryProcedures/__init__.py
@@ -4,12 +4,14 @@ from .sql_item_fatura import SQLItemFatura
 from .sql_item_fatura_trafego_detalhado import SQLItemFaturaTrafegoDetalhado
 from .sql_recarga_ocs import SQLRecargaOCS
 from .sql_crc_recuperacao_vas import SQLCRCRecuperacaoVAS
+from .sql_full_base import SQLFullBaseFatura
 
 class BigQueryProcedures(
     SQLItemFatura
     , SQLItemFaturaTrafegoDetalhado
     , SQLRecargaOCS
     , SQLCRCRecuperacaoVAS
+    , SQLFullBaseFatura
 ):
     
     def __init__(self, *, bq:BigQuery=None, project:str=None, token:str=None):
@@ -19,15 +21,3 @@ class BigQueryProcedures(
             self.bq = BigQuery(project, token)
         else:
             raise Exception("Sorry, conection or project and token are needed!")
-            
-    def proc_acum_full_base__replace_table(self):
-        query = """
-        create or replace table tim-sdbx-mktvas-b2ba.dm_prod.tbl_acum_full_base
-        cluster by dt_month_ref, dt_month_tarifacao, num_msisdn, num_app_id
-        as
-          select
-            *
-          from
-            dm_prod.vw_acum_full_base
-        """
-        self.bq.execute_query(query, commit=True)

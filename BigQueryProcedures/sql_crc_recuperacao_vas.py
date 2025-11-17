1be8f56be0238e7f155dd002a211bd8de3a4b93c
João Paulo Cautter <jcautter@gmail.com>
2025-11-17 12:34:49 -0300

    Update sql_crc_recuperacao_vas.py

--- BigQueryProcedures/sql_crc_recuperacao_vas.py
+++ BigQueryProcedures/sql_crc_recuperacao_vas.py
@@ -2,88 +2,36 @@ from .proc_log import ProcLog
 
 class SQLCRCRecuperacaoVAS:
 
-    def proc_crc_recuperacao_vas(self):
+    def proc_crc_recuperacao_vas(self, ano_mes:str):
+        print('proc crc vas -- Iniciado')
+        print('')
+        self.__create_car(ano_mes)
         self.__delete_cons()
         self.__insert_into_cons()
+        print('')
+        print('proc crc vas -- Finalizado')
         
+    def __create_car(self, ano_mes:str):
+        self._ProcLog__exec(
+            title = 'create car'
+            , module = 'crc vas'
+            , crud = 'create'
+            , name = 'create car'
+            , parm = {'ano_mes': ano_mes}
+        )
+    
     def __delete_cons(self):
         self._ProcLog__exec(
-            title = 'delete dm_prod.tbl_cons_crc_recuperacao_vas'
-            , query = """
-                delete
-                dm_prod.tbl_cons_crc_recuperacao_vas AS cons
-                where exists
-                (
-                    select
-                    dt_month_ref
-                    from
-                    (
-                        select distinct
-                        DATE(dt_month_ref) as dt_month_ref
-                        from
-                        dm_temp.tbl_car_crc_recuperacao_vas
-                    ) AS car
-                    where
-                    cons.dt_month_ref = DATE(dt_month_ref)
-                );
-            """
+            title = 'delete cons'
+            , module = 'crc vas'
+            , crud = 'delete'
+            , name = 'delete cons'
         )
     
     def __insert_into_cons(self):
         self._ProcLog__exec(
-            title = 'insert into dm_prod.tbl_cons_crc_recuperacao_vas'
-            , query = """
-                insert into
-                dm_prod.tbl_cons_crc_recuperacao_vas
-                select
-                dt_month_ref
-                , dt_ref
-                , dt_criacao
-                , dt_finalizacao
-                , num_protocolo
-                , num_fatura
-                , num_titular_msidn as num_titular_msisdn
-                , struct(
-                    str_segmento_contestacao
-                    , bit_reincidencia
-                    , bit_vas_only
-                    , str_m_reincidencia
-                ) stc_clasificacao
-                , struct(
-                    str_contestacao
-                    , str_motivo_contestacao
-                    , str_motivo_1
-                    , str_motivo_2
-                    , str_motivo_3
-                ) stc_motivo
-                , sum(num_valor) as num_valor
-                , count(*) as num_qtd
-                , array_agg(
-                    struct(
-                    id
-                    , num_appid
-                    , str_servico
-                    , num_valor
-                    )
-                ) arr_contestacao
-                from
-                dm_temp.tbl_car_crc_recuperacao_vas
-                group by
-                dt_month_ref
-                , dt_ref
-                , dt_criacao
-                , dt_finalizacao
-                , num_protocolo
-                , num_fatura
-                , num_titular_msidn
-                , str_segmento_contestacao
-                , bit_reincidencia
-                , bit_vas_only
-                , str_m_reincidencia
-                , str_contestacao
-                , str_motivo_contestacao
-                , str_motivo_1
-                , str_motivo_2
-                , str_motivo_3
-            """
+            title = 'insert cons'
+            , module = 'crc vas'
+            , crud = 'insert'
+            , name = 'insert cons'
         )
